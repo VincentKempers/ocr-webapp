9e211f36442eae708d1572260310e0dc85f6e6e1
VincentKempers <me@vncn.tk>
2019-10-08 15:35:08 +0200

    logs

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const voices = window.speechSynthesis.getVoices();
 const voice = voices.find(voice => voice.lang === 'nl-NL');
 
 if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = function(constraints) {
+	navigator.mediaDevices.getUserMedia = function (constraints) {
 
-    // First get ahold of the legacy getUserMedia, if present
-    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+		// First get ahold of the legacy getUserMedia, if present
+		var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    // Some browsers just don't implement it - return a rejected promise with an error
-    // to keep a consistent interface
-    if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-    }
+		// Some browsers just don't implement it - return a rejected promise with an error
+		// to keep a consistent interface
+		if (!getUserMedia) {
+			return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+		}
 
-    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-    return new Promise(function(resolve, reject) {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
-  }
+		// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+		return new Promise(function (resolve, reject) {
+			getUserMedia.call(navigator, constraints, resolve, reject);
+		});
+	}
 }
 
 function startProcess() {
@@ -49,18 +49,21 @@ function startProcess() {
 	read.addEventListener('click', readText);
 
 	function readText() {
-	  const ctx = canvas.getContext('2d');
+		const ctx = canvas.getContext('2d');
 
-	  ctx.drawImage(video, 0, 0);
+		ctx.drawImage(video, 0, 0);
 
 		Tesseract.recognize(
-		  ctx,
-		  'eng',
-		  { logger: m => console.log(m) }
+			ctx,
+			'eng',
+			{ logger: m => console.log(m) }
 		).then(result => {
 			if (result && result.text) {
+				console.log(result.text);
+				console.log(result.words)
 				const value = result.text;
-				talk(value);
+				console.log(value);
+				// talk(value);
 			}
 		});
 	}
